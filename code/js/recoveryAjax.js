7e80357224d7dfdcfa6446e77007add27a9923ed
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-21 19:13:54 +0200

    Added PHP script to be run

--- code/js/recoveryAjax.js
+++ code/js/recoveryAjax.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $("#passReq").click(function(event){
     var recoveryPhrase = $("#recoveryPhrase").val();
     var userName = $("#userName").val();
-    $("#stage").load('wordpressNotionAPI.php', {"recoveryPhrase":recoveryPhrase, "userName":userName} );
+    $("#stage").load('recoverPassword.php', {"recoveryPhrase":recoveryPhrase, "userName":userName} );
           });
 });
