6e487ae27a8928a72f0250af0f68160f2cfd54e8
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-21 19:06:06 +0200

    Added comments to explain what script does

--- code/js/conditionalLibrarianPass.js
+++ code/js/conditionalLibrarianPass.js
@@ -1,3 +1,5 @@
+// conditionally librarian passcode depending on whether or not
+// the data from accountType is librarian or not
 $(document).ready(function() {
     $('#passCode').hide();
     $('#passCodeLabel').hide();
