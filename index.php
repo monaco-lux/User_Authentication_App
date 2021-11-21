8078eda35efac821ec00a646b0c633842371ddbd
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-21 19:06:56 +0200

    Changed title to "Local Library CMS"

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $_SESSION['wasHome'] = "Yes";
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>Local Library: Sign in</title>
+<title>Local Library CMS: Sign in</title>
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 </head>
