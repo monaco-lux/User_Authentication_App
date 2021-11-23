dcd3c72773bc5c0d57124f1762a37b3f95a928b8
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-23 15:18:34 +0200

    check for librarian account creation

--- code/php/includes/signUpWriteBack.inc.php
+++ code/php/includes/signUpWriteBack.inc.php
@@ -19,9 +19,13 @@ $newSignup->password = $_POST['password'];
 $newSignup->accountType = $_POST['accountType'];
 $newSignup->passCode = $_POST['passCode'];
 
-// running errror handler and user signup
-$newSignup->signupUser();
+// running errror handler and user signup | if the passcode is incorrect they cannot create a librarian
+if($newSignup->accountType == "member" || $newSignup->passCode == $newSignup->secretCode)
+{
+  $newSignup->signupUser();
+  header("Location: ../../../index.php?error=none");
+} else {
+  header("Location: ../../../index.php?error=incorrectpasscode");
+}
 
-//go back to frontpage
-header("Location: ../../../index.php?error=none");
 ?>
