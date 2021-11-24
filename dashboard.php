92fc68a27838d92015d44bcc2738ea99ff7bed7d
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-24 12:59:14 +0200

    Added code to unset session and logout. Added logout button.

--- dashboard.php
+++ dashboard.php
@@ -21,6 +21,7 @@ session_start();
 <div class="w3-bar w3-black">
  <a href="index.php" class="w3-bar-item w3-button">Home</a>
  <a href="signUp.php" class="w3-bar-item w3-button">Sign up!</a>
+ <a href="" class="w3-bar-item w3-button w3-orange w3-right">Logout</a>
 </div>
 
 <div class="w3-container w3-section w3-teal w3-mobile">
