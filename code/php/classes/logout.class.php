151db4774a8e3d12cea3c09f0d22819bce802f38
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2021-11-29 13:10:29 +0200

    Logout class and function to logout and log it

---
+++ code/php/classes/logout.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class LogOut extends DbH
+{
+
+  private $userName;
+
+  public function __get($property) {
+    if (property_exists($this, $property)) {
+      return $this->$property;
+    }
+  }
+
+  public function __set($property, $value)
+  {
+      if (property_exists($this, $property)) {
+        $this->$property = $value;
+      }
+    }
+
+
+  public function logoutLogger($uid)
+  {
+      //record that user logged out and on what time
+    $logoutTime = "Logout: ".date('d-m-y h:i:s');
+    $stmt = $this->connect()->prepare('INSERT INTO session_login (username,session_id,login_logout) VALUES(?,?,?) ;');
+    if(!$stmt->execute([$uid,session_id(),$loginTime])) // if query doesnt work throw error
+    {
+      $stmt = null;
+      header("Location: ../../../index.php?error=couldnotupdatesession");
+      exit();
+    }
+  }
+
+
+}
+
+?>
--- code/php/includes/logout.inc.php
+++ code/php/includes/logout.inc.php
@@ -1,7 +1,18 @@
 <?php
 session_start();
+
+include "../classes/dbh.class.php";
+include "../classes/logout.class.php";
+
+// call logoutLogger function to record that user has logged out
+$logout = new LogOut();
+$logout->userName = $_SESSION['username']
+$logout->logoutLogger($logout->userName);
+
+// end session
 session_unset();
 session_destroy();
 
+// return back to login screen
 header("Location: ../../../index.php?error=loggedout");
 ?>
